c1e667e99ee30fc584ad0e187b5d648990a0d357
Arthur Hauck Dittz <arthur.hauck@dtidigital.com.br>
2024-02-11 21:06:44 -0300

    Adicionar arquivos de projeto.

---
+++ Empresa.SqlServer/dbo/Tables/Client.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Client] (
+    [IdClient]   INT           IDENTITY (1, 1) NOT NULL,
+    [NameClient] VARCHAR (120) NOT NULL,
+    [Suburb]     VARCHAR (60)  NOT NULL,
+    [Phone]      VARCHAR (15)  NOT NULL,
+    [Email]      VARCHAR (50)  NOT NULL,
+    CONSTRAINT [pk_id_autor] PRIMARY KEY CLUSTERED ([IdClient] ASC)
+);
+
---
+++ Empresa.SqlServer/dbo/Tables/Leads.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Leads] (
+    [Id]           INT             IDENTITY (100, 1) NOT NULL,
+    [Category]     VARCHAR (50)    NOT NULL,
+    [Description]  VARCHAR (100)   NOT NULL,
+    [Price]        DECIMAL (10, 2) NOT NULL,
+    [StatusId]     INT             NULL,
+    [IdClient]     INT             NULL,
+    [CreationDate] DATETIME        NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [verifica_preco] CHECK ([Price]>=(0)),
+    CONSTRAINT [fk_ClientId] FOREIGN KEY ([IdClient]) REFERENCES [dbo].[Client] ([IdClient]),
+    CONSTRAINT [fk_StatusId] FOREIGN KEY ([StatusId]) REFERENCES [dbo].[Status] ([Id])
+);
+
